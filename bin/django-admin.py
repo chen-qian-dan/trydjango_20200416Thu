1336369cb26e41977beec9be99b616838c5b3b02
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-04-16 16:12:00 +0930

    Install django 3.0.5

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/qianchen/Documents/QC19_03_DOING/git_20200109Thu/trydjango_20200416Thu/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
